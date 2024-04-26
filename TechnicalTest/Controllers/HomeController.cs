942fa1df9e3bd785b507efe3bb357ed5acefb637
Cuong Nguyen <dev.frontend1@mifivietnam.com>
2024-04-26 14:49:34 -0700

    Add project files.

---
+++ TechnicalTest/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+using TechnicalTest.Models;
+
+namespace TechnicalTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public HomeController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [EnableCors("AllowAnyOrigin")]
+        [HttpGet("/avatar")]
+        public async Task<IActionResult> GetProfileImage([FromQuery] string userIdentifier)
+        {
+            var url = string.Empty;
+            var lastCharacter = GetLastCharacter(userIdentifier);
+            switch (lastCharacter) {
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    url = $"https://my-json-server.typicode.com/ck-pacificdev/tech-test/images/{lastCharacter}";
+                    break;
+
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                    var lastDigit = int.Parse(lastCharacter);
+                    var image = await _dataContext.Images.FirstOrDefaultAsync(img => img.Id == lastDigit);
+                    if (image != null)
+                    {
+                        url = image.Url;
+                    }
+
+                    break;
+
+                default:
+                    var vowelMatch = Regex.Match(userIdentifier, "[aeiou]");
+                    if (vowelMatch.Success) // at least one vowel character
+                    {
+                        char firstVowel = vowelMatch.Value[0];
+                        url = $"https://api.dicebear.com/8.x/pixel-art/png?seed={firstVowel}&size=150"; // Build url from the first vowel
+                    }
+
+                    if (Regex.IsMatch(userIdentifier, "[^a-zA-Z0-9]")) // non-alphanumeric character
+                    {
+                        Random random = new Random();
+                        url = $"https://api.dicebear.com/8.x/pixel-art/png?seed={random.Next()}&size=150";
+                    }
+
+                    break;
+            }
+
+            if (string.IsNullOrEmpty(url))
+            {
+                url = $"https://api.dicebear.com/8.x/pixel-art/png?seed=default&size=150";
+            }
+
+            return Ok(new { URL = url });
+        }
+
+        private string GetLastCharacter(string userIdentifier) {
+            if (string.IsNullOrEmpty(userIdentifier))
+            {
+                return string.Empty;
+            }
+
+            return userIdentifier.Last().ToString();
+        }
+    }
+}
---
+++ TechnicalTest/Models/Image.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechnicalTest.Models;
+
+public partial class Image
+{
+    public int Id { get; set; }
+
+    public string Url { get; set; } = null!;
+}
